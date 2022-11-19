d8590e2cf027157a0553512f7699d032bf699e33
Abrar <abrarhabib285@gamil.com>
2022-11-19 13:59:58 -0500

    fixed form submission error

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-const username = document.querySelector('#name');
-console.log(username);
+
+const Submit = () =>{
+    const username = document.getElementById('name');
+    console.log(username.value);
+}
+class Event{
+    constructor(eventName, date, time, description) {
+       this.eventName = eventName,
+       this.time = time,
+       this.description = description,
+       this.date = date,
+       this.schedule
+    }
+    renameEvent() {
+        const newEventName = prompt("What is the new event name?");
+        this.event = newEventName;
+        return;
+    }
+}
 
 
-
-// class Event{
-//     constructor(eventName, date, time, description) {
-//        this.eventName = eventName,
-//        this.time = time,
-//        this.description = description,
-//        this.date = date,
-//        this.schedule
-//     }
-//     renameEvent() {
-//         const newEventName = prompt("What is the new event name?");
-//         this.event = newEventName;
-//         return;
-//     }
-// }
+class User extends Event{
+    constructor() {
+        const username = document.querySelector("#name").value; 
+        this.username = username;
+        console.log(this.username);
+        this.schedule = {};
+    }
+    add(Event) {
+        this.schedule[event.name] = event;
+        console.log(event); //debug
+    }
+}
 
 
-// class User extends Event{
-//     constructor() {
-//         const username = document.querySelector("#name").value; 
-//         this.username = username;
-//         console.log(this.username);
-//         this.schedule = {};
-//     }
-//     add(Event) {
-//         this.schedule[event.name] = event;
-//         console.log(event); //debug
-//     }
-// }
+class Schedule extends User {
+    constructor() {
+        console.log("hello");
 
+    }
+}
 
-// class Schedule extends User {
-//     constructor() {
-//         console.log("hello");
+class Lecture extends Event {
+    constructor(professor, professorEmail) {
+        //super all of the things from the event class
+        super(lectureName, date, time, description);
+        this.professor = professor,
+        this.professorEmail = professorEmail
+    }
 
-//     }
-// }
-
-// class Lecture extends Event {
-//     constructor(professor, professorEmail) {
-//         //super all of the things from the event class
-//         super(lectureName, date, time, description);
-//         this.professor = professor,
-//         this.professorEmail = professorEmail
-//     }
-
-// }
+}
 
 
 
